65bdaf8887d728942c824c9d05f7f6a8553f0cef
wa-sa07 <118840033+wa-sa07@users.noreply.github.com>
2024-09-27 17:15:30 -0300

    Adicionando novo Projeto
    Adicionado a pasta CADASTRO-USUARIOS/LOGIN
    Funcionalidades ao novo projeto

---
+++ CADASTRO-USUARIOS/LOGIN/config.php
@@ -0,0 +1,18 @@
+<?php
+    // Definir as variáveis de conexão
+    $dbHost = "localhost";
+    $dbUser = "root";
+    $dbPassword = '';  // Insira sua senha aqui, ou deixe vazio se não houver senha
+    $dbName = 'formulario-teste';  // Verifique se o nome está correto
+    $dbPort = 3307;  // Porta configurada no arquivo de configuração MySQL
+
+    // Conectar ao banco de dados MySQL
+    $conexao = new mysqli($dbHost, $dbUser, $dbPassword, $dbName, $dbPort);
+
+    // Verificar a conexão
+    if ($conexao->connect_error) {
+        die("Falha na conexão: " . $conexao->connect_error);
+    } else {
+        echo "Conexão efetuada com sucesso!";
+    }
+?>
